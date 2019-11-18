fix scale on eval and cal recall+precision

keras_retinanet/bin/train.py
@@ -61,7 +61,7 @@ from ..utils.colors import label_color
 import glob
 
 labels_to_names = {0: 'pigeon'}
-
+wandb.init(project="pigeon")
 def makedirs(path):
     # Intended behavior: try to create the directory,
     # pass if the directory exists already, fails otherwise.
@@ -141,6 +141,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
 class log_image_callback(Callback):
     def on_epoch_end(self, epoch, logs={}):
         images = glob.glob('data4eval/non-pigeon/*.jpg')
+        images += glob.glob('data4eval/non-pigeon/*.png')
         inference_model = models.convert_model(self.model)
 
         # images are of differrent dimensions, so couldn't get batch
@@ -159,12 +160,14 @@ class log_image_callback(Callback):
 
         # preprocess image for network
         image = preprocess_image(image)
-        image, scale = resize_image(image)
+        image, scale = resize_image(image, min_side=700, max_side=700)
 
         # process image
         start = time.time()
         boxes, scores, labels = inference_model.predict_on_batch(np.expand_dims(image, axis=0))
-        print("processing time: ", time.time() - start)
+        boxes /= scale
+        pst = time.time() - start
+        print("processing time: ", pst)
         annotations = []
         for box, score, label in zip(boxes[0], scores[0], labels[0]):
             # scores are sorted so we can break
@@ -182,7 +185,7 @@ class log_image_callback(Callback):
         annotation_str = ', '.join(annotations)
         if len(annotation_str) == 0:
             annotation_str = "Still Learning!"
-        wandb.log({image_name: [wandb.Image(draw, caption=annotation_str)]}, commit=False)
+        wandb.log({image_name: [wandb.Image(draw, caption='Processing Time: {pst}, {anno}'.format(pst= pst, anno = annotation_str))]}, commit=False)
 
 def create_callbacks(model, training_model, prediction_model, validation_generator, args):
     """ Creates the callbacks to use during training.
@@ -545,7 +548,7 @@ def main(args=None):
         )
 
     # print model summary
-    print(model.summary())
+    # print(model.summary()) 
 
     # this lets the generator compute backbone layer shapes using the actual backbone model
     if 'vgg' in args.backbone or 'densenet' in args.backbone:
@@ -580,6 +583,6 @@ def main(args=None):
 
 
 if __name__ == '__main__':
-    wandb.init(project="pigeon")
+
     model = main()
     model.save(os.path.join(wandb.run.dir, "model.h5"))

keras_retinanet/utils/eval.py
@@ -14,15 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from .anchors import compute_overlap
-from .visualization import draw_detections, draw_annotations
-
-import keras
-import numpy as np
 import os
+import pprint
 
 import cv2
+import keras
+import numpy as np
 import progressbar
+
+from .anchors import compute_overlap
+from .visualization import draw_annotations, draw_detections
+
 assert(callable(progressbar.progressbar)), "Using wrong progressbar module, install 'progressbar2' instead."
 
 
@@ -240,24 +242,44 @@ def evaluate(
         # compute false positives and true positives
         false_positives = np.cumsum(false_positives)
         true_positives  = np.cumsum(true_positives)
-        # print('num:',num_annotations)
-        # print('false',false_positives, len(false_positives))
-        # print('true',true_positives, len(true_positives))
+        
+        np_max = np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
+        num_detects = len(true_positives)
+        
+        temp_confusion = {}
+        temp_confusion[label] = {'true_positives': true_positives[-1],
+                           'false_positives': false_positives[-1],
+                            'num_annotations': num_annotations,
+                            'num_detects': num_detects,
+                            }
+        
+        
+        
+        temp_confusion[label]['recall'] =  true_positives[-1]/num_annotations
+        temp_confusion[label]['precision'] =  true_positives[-1]/num_detects
+        
+        pprint.pprint(temp_confusion)
+        
+        
+        # print('num anno:', num_annotations)
+        # print('false', len(false_positives), false_positives)
+        # print('true', len(true_positives), true_positives)
+        # print('np_max', len(np_max), np_max)
         # compute recall and precision
         recall    = true_positives / num_annotations
-        precision = true_positives / np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
+        precision = true_positives / np_max
         # print('recall', recall, len(recall), np.sum(recall)/len(recall))
 
-        average_recall = np.sum(recall)/len(recall) if len(recall) > 0 else 0
-        average_precision_t = np.sum(precision)/len(precision) if len(precision) > 0 else 0
-        print('avg_racall', average_recall)
-        print('avg_precision', average_precision_t)
+        # average_recall = np.sum(recall)/len(recall) if len(recall) > 0 else 0
+        # average_precision_t = np.sum(precision)/len(precision) if len(precision) > 0 else 0
+        # print('avg_racall', average_recall)
+        # print('avg_precision', average_precision_t)
         # print('precision', precision, len(precision))
         # compute average precision
         average_precision  = _compute_ap(recall, precision)
         average_precisions[label] = average_precision, num_annotations
 
-
-        prerecall[label] = average_recall, average_precision_t, num_annotations
-    # print('aps:', average_precisions)
-    return average_precisions, prerecall
+        
+        # prerecall[label] = average_recall, average_precision_t, num_annotations
+        # print('aps:', average_precisions)
+    return average_precisions, temp_confusion